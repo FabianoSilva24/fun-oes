7e4a8757d8132c97cbf7827b7035bb40f7fff116
Fabiano Silva <fabiano.jaragua24@hotmail.com>
2018-08-22 12:27:12 -0300

    Exercicio de função

---
+++ funcao.js
@@ -0,0 +1,46 @@
+// exercicio 1
+
+// let lista =[2,1,5,4,3];
+// function achar(lista){
+//     console.log(lista);
+//     for(let item of lista){
+//         if (item === 3){ 
+//             return true;
+//         }
+//     }
+//     return false
+// } 
+// achar(lista);
+
+
+
+// exercicio 2
+
+// let frase = prompt("digite uma frase ");
+// let letra = prompt("digite uma letra");
+// function acharl (frase){
+//     for(let item in frase){
+//         console.log(frase.split(letra).length-1);
+//     }
+// }
+
+// acharl(frase); 
+
+
+
+//exercicio 3
+
+// let frase = prompt("digite uma frase ");
+// let letra = prompt("digite uma letra");
+
+// function acharl (frase){
+//     for(let item in frase){
+//         console.log(frase.split(letra).length-1);
+//     }
+// }
+
+// acharl(frase);
+
+
+
+
